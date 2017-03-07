96374accb86aefbbde14c02a8198160a1e0bf412
hprofit <holdenprofit@gmail.com>
2017-03-06 19:51:27 -0500

    Added Sky.

--- src/states/GameState.js
+++ src/states/GameState.js
@@ -1,17 +1,15 @@
+import Sky from '../objects/Environment/Sky';
 import Player from '../objects/Player/Player';
 
 export default class GameState extends Phaser.State {
     constructor() {
         super();
-
         this.player = null;
         this.background = null;
     }
 
     create() {
-        this.background = new Phaser.Image(this.game, 0, 0, 'bg_1');
-        this.game.add.existing(this.background);
-
+        this.background = new Sky(this.game);
         this.player = new Player(this.game, 100, 100);
     }
 
